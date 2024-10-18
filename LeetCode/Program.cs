cbc691a534e4415bd2bc6c17f9ea2739d73432c3
Зюзяков Павел <zyuzyakov_pk@astralnalog.ru>
2024-10-19 00:53:01 +1000

    easy 69: Дано неотрицательное целое число x, вернуть квадратный корень x округленного вниз до ближайшего целого числа.
    ref: https://leetcode.com/problems/sqrtx/description/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,94 +1,37 @@
 ﻿using System;
-using System.Collections;
 
 namespace LeetCode
 {
-    // hard 4: Даны два отсортированных массива nums1 и nums2 размером m и n соответственно, вернуть медиану двух отсортированных массивов.
-    // Общая сложность времени выполнения должна быть O(log (m+n)).
-    // https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+    // easy 69: Дано неотрицательное целое число x, вернуть квадратный корень x округленного вниз до ближайшего целого числа.
+    // ref: https://leetcode.com/problems/sqrtx/description/
     public static class Program
     {
         public static void Main(string[] args)
         {
-            var result = FindMedianSortedArrays([2], [2]);
-
-            Console.WriteLine(result);
-
+            Console.WriteLine(MySqrt(2147395599));
             Console.ReadKey();
         }
 
-        static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        static int MySqrt(int x)
         {
-            if (IsExtremeConditions(nums1, nums2, out double result))
-                return result;
+            if (x == 0)
+                return 0;
 
-            var isPairMedian = (nums1.Length + nums2.Length) % 2 == 0;
-            var medianIndex = (nums1.Length + nums2.Length) / 2 + 1;
+            if (x == 1)
+                return 1;
 
-            var leftEnumerator = nums1.GetEnumerator();
-            var rightEnumerator = nums2.GetEnumerator();
-            leftEnumerator.MoveNext();
-
-            int currentIndex = 0;
-            int currentValue = (int)leftEnumerator.Current;
-            int prevValue = 0;
-
-            while (currentIndex < medianIndex)
+            uint val = 1;
+            while (true)
             {
-                prevValue = currentValue;
-
-                currentValue = GetSmallerValue(ref leftEnumerator, ref rightEnumerator);
-
-                currentIndex++;
-            }
-
-            return isPairMedian ? (double)(currentValue + prevValue) / 2 : currentValue;
-        }
-
-        static int GetSmallerValue(ref IEnumerator current, ref IEnumerator second)
-        {
-            if (second.MoveNext())
-            {
-                bool currentSmaller = (int)current.Current < (int)second.Current;
-                if (currentSmaller)
-                {
-                    var newSecond = current;
-                    current = second;
-                    second = newSecond;
-                    return (int)newSecond.Current;
-                }
-                else
-                    return (int)second.Current;
-            }
-
-            var currentValue = (int)current.Current;
-            current.MoveNext();
-            return currentValue;
-        }
-
-        static bool IsExtremeConditions(int[] nums1, int[] nums2, out double result)
-        {
-            bool hasEmptyArray = nums1.Length == 0 || nums2.Length == 0;
-            if (hasEmptyArray)
-            {
-                var notEmptyArray = nums1.Length == 0 ? nums2 : nums1;
-
-                var isPairMedian = notEmptyArray.Length % 2 == 0;
-                if (isPairMedian)
-                {
-                    var rightIndex = notEmptyArray.Length / 2;
-                    result = (double)(notEmptyArray[rightIndex - 1] + notEmptyArray[rightIndex]) / 2;
-                    return true;
-                }
+                uint next = val + 1;
+                if (next * next > x)
+                    return (int)val;
                 else
                 {
-                    result = notEmptyArray[notEmptyArray.Length / 2];
-                    return true;
+                    val = next;
+                    continue;
                 }
             }
-
-            result = 0;
-            return false;
         }
     }
 }
