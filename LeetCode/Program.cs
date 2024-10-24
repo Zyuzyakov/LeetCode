347e64474b6c3783ff57ba5a021ecfa598e28708
Зюзяков Павел <zyuzyakov_pk@astralnalog.ru>
2024-10-24 22:23:15 +1000

    easy 704: Двоичный поиск.
    Дан массив целых чисел nums, отсортированный по возрастанию, и целое число target.
    Напишите функцию для поиска target в nums. Если target существует, то верните его индекс.
    В противном случае верните -1.
    
    ref: https://leetcode.com/problems/binary-search/description/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,40 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace LeetCode
 {
-    // medium 3: Самая длинная подстрока без повторяющихся символов.
-    // ref: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    // easy 704: Двоичный поиск.
+    // Дан массив целых чисел nums, отсортированный по возрастанию, и целое число target.
+    // Напишите функцию для поиска target в nums. Если target существует, то верните его индекс.
+    // В противном случае верните -1.
+    // ref: https://leetcode.com/problems/binary-search/description/
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(LengthOfLongestSubstring("qrsvbspk")); //qrsvb
-
+            Console.WriteLine(Search([-1, 0, 3, 5, 9, 12], 9));
             Console.ReadKey();
         }
 
-        static int LengthOfLongestSubstring(string s)
+        static int Search(int[] nums, int target)
         {
-            var queue = new Queue<char>();
-            int max = 0;
-            int current = 0;
+            int startIndex = 0;
+            int endIndex = nums.Length - 1;
 
-            while (current < s.Length)
+            while (endIndex >= startIndex)
             {
-                if (!queue.Contains(s[current]))
-                {
-                    queue.Enqueue(s[current]);
-                    max = Math.Max(max, queue.Count);
-                    current++;
-                }
+                int middleIndex = (startIndex + endIndex) / 2;
+                if (nums[middleIndex] == target)
+                    return middleIndex;
+
+                if (nums[middleIndex] < target)
+                    startIndex = middleIndex + 1;
                 else
-                {
-                    queue.Dequeue();
-                }
+                    endIndex = middleIndex - 1;
             }
 
-            return max;
+            return -1;
         }
     }
 }
