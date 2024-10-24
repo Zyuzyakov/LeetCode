f6bc32c89e6fd19be056183bd7a8b578e0e4ab0c
Зюзяков Павел <zyuzyakov_pk@astralnalog.ru>
2024-10-25 00:14:20 +1000

    medium 74: Поиск в двумерной матрице.
    ref: https://leetcode.com/problems/search-a-2d-matrix/description/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,37 +2,59 @@
 
 namespace LeetCode
 {
-    // easy 704: Двоичный поиск.
-    // Дан массив целых чисел nums, отсортированный по возрастанию, и целое число target.
-    // Напишите функцию для поиска target в nums. Если target существует, то верните его индекс.
-    // В противном случае верните -1.
-    // ref: https://leetcode.com/problems/binary-search/description/
+    // medium 74: Поиск в двумерной матрице.
+    // Вам дана m x nцелочисленная матрица matrix со следующими двумя свойствами:
+    //  -Каждая строка сортируется в неубывающем порядке.
+    //  -Первое целое число каждой строки больше последнего целого числа предыдущей строки.
+    // Дано целое число target, вернуть, true если target находится в пределах matrix, в противном случае false.
+    // Вам необходимо написать решение по O(log(m * n))временной сложности.
+    // ref: https://leetcode.com/problems/search-a-2d-matrix/description/
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(Search([-1, 0, 3, 5, 9, 12], 9));
+            Console.WriteLine(SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3));
             Console.ReadKey();
         }
 
-        static int Search(int[] nums, int target)
+        static bool SearchMatrix(int[][] matrix, int target)
         {
-            int startIndex = 0;
-            int endIndex = nums.Length - 1;
+            int lineEndIndex = matrix[0].Length - 1;
+            int startLine = 0;
+            int endLine = matrix.Length - 1;
+            int middleLine = 0;
 
-            while (endIndex >= startIndex)
+            if (target < matrix[0][0] || target > matrix[endLine][lineEndIndex])
+                return false;
+
+            while (endLine >= startLine)
             {
-                int middleIndex = (startIndex + endIndex) / 2;
-                if (nums[middleIndex] == target)
-                    return middleIndex;
-
-                if (nums[middleIndex] < target)
-                    startIndex = middleIndex + 1;
+                middleLine = (startLine + endLine) / 2;
+                if (target > matrix[middleLine][lineEndIndex])
+                    startLine = middleLine + 1;
+                else if (target < matrix[middleLine][0])
+                    endLine = middleLine - 1;
                 else
-                    endIndex = middleIndex - 1;
+                    break;
             }
 
-            return -1;
+            var findLine = matrix[middleLine];
+            startLine = 0;
+            endLine = lineEndIndex;
+
+            while (endLine >= startLine)
+            {
+                middleLine = (startLine + endLine) / 2;
+                if (target == findLine[middleLine])
+                    return true;
+
+                if (target > findLine[middleLine])
+                    startLine = middleLine + 1;
+                else
+                    endLine = middleLine - 1;
+            }
+
+            return false;
         }
     }
 }
