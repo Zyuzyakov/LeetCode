f3ce8b1eab9229010c731d36cec0f24535cb3e9d
Зюзяков Павел <zyuzyakov_pk@astralnalog.ru>
2024-10-26 00:24:32 +1000

    hard 301: удалить неверные скобки.
    Дана строка s, содержащая скобки и буквы. Удалите минимальное количество недопустимых скобок, чтобы сделать входную строку допустимой.
    Верните список уникальных строк, которые являются допустимыми с минимальным количеством удалений. Вы можете вернуть ответ в любом порядке.
    
    ref: https://leetcode.com/problems/remove-invalid-parentheses/description/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,60 +1,111 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace LeetCode
 {
-    // medium 74: Поиск в двумерной матрице.
-    // Вам дана m x nцелочисленная матрица matrix со следующими двумя свойствами:
-    //  -Каждая строка сортируется в неубывающем порядке.
-    //  -Первое целое число каждой строки больше последнего целого числа предыдущей строки.
-    // Дано целое число target, вернуть, true если target находится в пределах matrix, в противном случае false.
-    // Вам необходимо написать решение по O(log(m * n))временной сложности.
-    // ref: https://leetcode.com/problems/search-a-2d-matrix/description/
+    // hard 301: удалить неверные скобки.
+    // Дана строка s, содержащая скобки и буквы. Удалите минимальное количество недопустимых скобок, чтобы сделать входную строку допустимой.
+    // Верните список уникальных строк, которые являются допустимыми с минимальным количеством удалений. Вы можете вернуть ответ в любом порядке.
+    // ref: https://leetcode.com/problems/remove-invalid-parentheses/description/
     public static class Program
     {
+        private static HashSet<string> _hSet = new HashSet<string>();
+        private static int _maxLength = 0;
+        private static int countBigO = 0;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine(SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3));
+            string param = "(((((((((((((((aaaa)))))";
+            Console.WriteLine("Исходная строка: " + param);
+            RemoveInvalidParentheses(param).Select(x => { Console.WriteLine(x); return x; }).ToList(); // ["(a())()","(a)()()"]
+            Console.WriteLine("Итераций: " + countBigO);
             Console.ReadKey();
         }
 
-        static bool SearchMatrix(int[][] matrix, int target)
+        static IList<string> RemoveInvalidParentheses(string s)
         {
-            int lineEndIndex = matrix[0].Length - 1;
-            int startLine = 0;
-            int endLine = matrix.Length - 1;
-            int middleLine = 0;
+            Search(s, 0, new LineParentheses());
 
-            if (target < matrix[0][0] || target > matrix[endLine][lineEndIndex])
-                return false;
+            return _hSet.ToList();
+        }
 
-            while (endLine >= startLine)
+        static void Search(string str, int index, LineParentheses line)
+        {
+            if (!line.IsValid && (line.Open - line.Close) > str.Length - index)
+                return;
+
+            countBigO++;
+
+            if (line.IsValid)
+                AddToHashSet(line);
+
+            if (index >= str.Length)
+                return;
+
+            var currentChar = str[index++];
+            var newLine = new LineParentheses(line, currentChar);
+            if (newLine.IsBad)
+                newLine = new LineParentheses();
+
+            if (newLine.ToString() != line.ToString())
             {
-                middleLine = (startLine + endLine) / 2;
-                if (target > matrix[middleLine][lineEndIndex])
-                    startLine = middleLine + 1;
-                else if (target < matrix[middleLine][0])
-                    endLine = middleLine - 1;
-                else
-                    break;
+                Search(str, index, line);
+                Search(str, index, newLine);
+            }
+            else
+            {
+                Search(str, index, line);
+            }
+        }
+
+        static void AddToHashSet(LineParentheses line)
+        {
+            if (line.Length > _maxLength)
+            {
+                _maxLength = line.Length;
+                _hSet.Clear();
+                _hSet.Add(line.ToString());
+            }
+            else if (line.Length == _maxLength)
+            {
+                _hSet.Add(line.ToString());
+            }
+        }
+
+        private class LineParentheses
+        {
+            public StringBuilder Line { get; private set; } = new StringBuilder();
+            public int Length => Line.Length;
+            public bool IsValid => Open == Close;
+            public bool IsBad => Close > Open;
+            public int Open { get; private set; }
+            public int Close { get; private set; }
+
+            public LineParentheses() { }
+            public LineParentheses(LineParentheses cloneFrom, char c)
+            {
+                Open = cloneFrom.Open;
+                Close = cloneFrom.Close;
+                Line.Append(cloneFrom.ToString());
+                Add(c);
             }
 
-            var findLine = matrix[middleLine];
-            startLine = 0;
-            endLine = lineEndIndex;
-
-            while (endLine >= startLine)
+            public void Add(char c)
             {
-                middleLine = (startLine + endLine) / 2;
-                if (target == findLine[middleLine])
-                    return true;
+                if (IsBad)
+                    throw new Exception("Строка недопустима.");
 
-                if (target > findLine[middleLine])
-                    startLine = middleLine + 1;
-                else
-                    endLine = middleLine - 1;
+                if (c == '(')
+                    Open++;
+                else if (c == ')')
+                    Close++;
+
+                Line.Append(c);
             }
 
-            return false;
+            public override string ToString() => Line.ToString();
         }
     }
 }
