9d1fe641a1f66f333b6232f6a95b74a9b4495669
Зюзяков Павел <zyuzyakov_pk@astralnalog.ru>
2024-10-28 16:35:22 +1000

    medium 5: Самая длинная палиндромная подстрока.
    Дана строка s, вернуть самую длинную палиндромную подстроку в s.
    
    ref: https://leetcode.com/problems/longest-palindromic-substring/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,111 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LeetCode
 {
-    // hard 301: удалить неверные скобки.
-    // Дана строка s, содержащая скобки и буквы. Удалите минимальное количество недопустимых скобок, чтобы сделать входную строку допустимой.
-    // Верните список уникальных строк, которые являются допустимыми с минимальным количеством удалений. Вы можете вернуть ответ в любом порядке.
-    // ref: https://leetcode.com/problems/remove-invalid-parentheses/description/
+    // medium 5: Самая длинная палиндромная подстрока.
+    // Дана строка s, вернуть самую длинную палиндромную подстроку в s.
+    // ref: https://leetcode.com/problems/longest-palindromic-substring/
     public static class Program
     {
-        private static HashSet<string> _hSet = new HashSet<string>();
-        private static int _maxLength = 0;
-        private static int countBigO = 0;
-
         public static void Main(string[] args)
         {
-            string param = "(((((((((((((((aaaa)))))";
-            Console.WriteLine("Исходная строка: " + param);
-            RemoveInvalidParentheses(param).Select(x => { Console.WriteLine(x); return x; }).ToList(); // ["(a())()","(a)()()"]
-            Console.WriteLine("Итераций: " + countBigO);
+            Console.WriteLine(LongestPalindrome("xaabacxcabaaxcabaax"));
+            Console.WriteLine(_countIn);
             Console.ReadKey();
         }
 
-        static IList<string> RemoveInvalidParentheses(string s)
-        {
-            Search(s, 0, new LineParentheses());
+        static string _maxPalindrome = "";
+        static UInt128 _countIn = 0;
 
-            return _hSet.ToList();
+        static string LongestPalindrome(string s)
+        {
+            for (int startIndex = 0; startIndex < s.Length; startIndex++)
+            {
+                for (int endIndex = startIndex; endIndex < s.Length; endIndex++)
+                {
+                    _countIn++;
+
+                    int strLength = endIndex - startIndex + 1;
+                    var subStr = s.Substring(startIndex, strLength);
+                    if (subStr.Length > _maxPalindrome.Length && IsPalindrome(subStr))
+                        _maxPalindrome = subStr;
+                }
+            }
+
+            return _maxPalindrome;
         }
 
-        static void Search(string str, int index, LineParentheses line)
+        static bool IsPalindrome(string str)
         {
-            if (!line.IsValid && (line.Open - line.Close) > str.Length - index)
-                return;
+            int start = 0;
+            int end = str.Length - 1;
 
-            countBigO++;
-
-            if (line.IsValid)
-                AddToHashSet(line);
-
-            if (index >= str.Length)
-                return;
-
-            var currentChar = str[index++];
-            var newLine = new LineParentheses(line, currentChar);
-            if (newLine.IsBad)
-                newLine = new LineParentheses();
-
-            if (newLine.ToString() != line.ToString())
+            while (start <= end)
             {
-                Search(str, index, line);
-                Search(str, index, newLine);
-            }
-            else
-            {
-                Search(str, index, line);
-            }
-        }
-
-        static void AddToHashSet(LineParentheses line)
-        {
-            if (line.Length > _maxLength)
-            {
-                _maxLength = line.Length;
-                _hSet.Clear();
-                _hSet.Add(line.ToString());
-            }
-            else if (line.Length == _maxLength)
-            {
-                _hSet.Add(line.ToString());
-            }
-        }
-
-        private class LineParentheses
-        {
-            public StringBuilder Line { get; private set; } = new StringBuilder();
-            public int Length => Line.Length;
-            public bool IsValid => Open == Close;
-            public bool IsBad => Close > Open;
-            public int Open { get; private set; }
-            public int Close { get; private set; }
-
-            public LineParentheses() { }
-            public LineParentheses(LineParentheses cloneFrom, char c)
-            {
-                Open = cloneFrom.Open;
-                Close = cloneFrom.Close;
-                Line.Append(cloneFrom.ToString());
-                Add(c);
+                if (str[start] != str[end])
+                    return false;
+                start++;
+                end--;
             }
 
-            public void Add(char c)
-            {
-                if (IsBad)
-                    throw new Exception("Строка недопустима.");
-
-                if (c == '(')
-                    Open++;
-                else if (c == ')')
-                    Close++;
-
-                Line.Append(c);
-            }
-
-            public override string ToString() => Line.ToString();
+            return true;
         }
     }
 }
